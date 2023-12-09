509a4430a0f431490a2ab0711989ed6341462b3d
kavindu09611 <kavinduwickramasinghe1920@gmail.com>
2023-12-10 02:22:43 +0530

    Add initial color palettes and designed background of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import './App.css'
+import styled from 'styled-components'
+import bg from './bg2.png'
+import bg2 from './grid.png'
 
 function App() {
   
 
   return (
-    <>
-     <div className="App"></div>
-    </>
+ 
+    
+    
+    
+     <AppStyled bg={bg} grid={bg2}>
+      <div className="grid"></div>
+
+     </AppStyled>
+  
   )
 }
+
+const AppStyled = styled.div`
+
+min-height:100vh;
+background-color:slateblue;
+background-image:url(${props => props.bg});
+background-repeat:no-repeat;
+background-size:cover;
+position:relative;
+
+.grid{
+  position:absolute;
+  width:100%;
+  height:100%;
+  background-image:url(${props => props.grid });
+  background-repeat:repeat;
+  z-index:0;
+}
+
+`;
 
 export default App
---
+++ src/Components/Palettes.jsx
@@ -0,0 +1,11 @@
+
+
+function Palettes() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Palettes
